01924a70dc46bd7cb618605cc76407f85d4f34ac
Peggy Griffin <seishingithub@gmail.com>
2014-06-29 14:06:41 -0600

    whitespace removal

--- app/controllers/ecards_controller.rb
+++ app/controllers/ecards_controller.rb
@@ -8,14 +8,3 @@ class EcardsController < ApplicationController
     @ecard = Ecard.new
   end
 end
-
-
-
-#class PhotoSelectController < ApplicationController
-#
-#  def index
-#    reddit_api = RedditApi.new
-#    @earth_porn = reddit_api.earth_porn
-#  end
-#
-#end
