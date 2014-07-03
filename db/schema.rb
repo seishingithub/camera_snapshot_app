5ff17029cf9d4e99ce8d92d11bd8c8ff3c91c6fb
Peggy Griffin <seishingithub@gmail.com>
2014-07-03 11:01:40 -0600

    Added User_id column to Messages Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703014706) do
+ActiveRecord::Schema.define(version: 20140703164945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140703014706) do
     t.integer  "photo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "photos", force: true do |t|
