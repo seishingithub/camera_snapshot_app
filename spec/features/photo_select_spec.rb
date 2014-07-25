86f7fae057d2163948657837099369edfcac4330
Peggy Griffin <seishingithub@gmail.com>
2014-07-25 16:19:50 -0600

    Changed photo image on homepage and began styling on '123' graphic

--- spec/features/photo_select_spec.rb
+++ spec/features/photo_select_spec.rb
@@ -5,7 +5,7 @@ feature 'Managing Photo Selections' do
   scenario 'a user can visit the Photo Select page' do
     VCR.use_cassette('features/photo_select/show') do
       visit '/'
-      click_on 'Photo Select'
+      find("a.photo_select_btn").click
       expect(page).to have_content 'Photo Select'
     end
   end
